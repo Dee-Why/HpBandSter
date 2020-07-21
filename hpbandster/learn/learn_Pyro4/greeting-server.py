aba92d908c851df53a277ce7b9c2bac1170d1e27
Dee-Why <1800011078@pku.edu.cn>
2020-07-21 23:18:24 +0800

    use the "nameserver" for the first time

--- hpbandster/learn/learn_Pyro4/greeting-server.py
+++ hpbandster/learn/learn_Pyro4/greeting-server.py
@@ -4,10 +4,12 @@ import Pyro4
 class GreetingMaker(object):
     def get_fortune(self, name):
         return "Hello, {0}. Here is your fortune message:\n" \
-               "Behold the warranty -- the bold print giveth and the fine print taketh away.".format(name)
+               "Tomorrow's lucky number is 12345678.".format(name)
 
 daemon = Pyro4.Daemon()                # make a Pyro daemon
+ns = Pyro4.locateNS()                  # find the name server
 uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
+ns.register("example.greeting", uri)   # register the object with a name in the name server
 
-print("Ready. Object uri =", uri)      # print the uri so we can use it in the client later
+print("Ready.")
 daemon.requestLoop()                   # start the event loop of the server to wait for calls
